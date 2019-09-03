3f5505ca8611750dc7a3f344b872ab9c5824a65d
ysz <easy_water@126.com>
2019-09-03 17:00:10 +0800

    可编辑表格完成

--- src/components/tb.vue
+++ src/components/tb.vue
@@ -3,11 +3,11 @@
       <section class="normal-bg">
         <h3 class="item-head">可编辑表格</h3>
         <section class="item-content">
-          <Table :columns="cityColumns" border :data="tbData"></Table>
+          <Table :columns="tbColumns" border :data="tbData"></Table>
         </section>
         <section class="btn-group">
             <Button type="info">取消</Button>
-            <Button type="success" style="margin-left: 24px;">提交</Button>
+            <Button type="success" style="margin-left: 24px;" @click="handleSubmit">提交</Button>
         </section>
       </section>
     </section>
@@ -18,13 +18,28 @@ export default {
   name: "editTb",
   data() {
     return {
-      cityColumns: [
-        //全市警情
+      tbColumns: [ //主要是render不同
         {
           title: "姓名",
           key: "name",
           render: (h, params) => {
-            return this.renderInput(h, params, 'name', 'tbDataCopy')        
+            if (params.row.edit) {
+              return h("Input", {
+                  props: {
+                      value: params.row.name,
+                  },
+                  attrs: {
+                      id: `name_${params.index}`
+                  },
+                  on: {
+                      "on-blur": () => {
+                        this.tbDataCopy[params.index].name = document.querySelector(`#name_${params.index} input`).value;
+                      }
+                  }
+              });
+            } else {
+                return h("span", params.row.name);
+            }  
           }
         },
         {
@@ -32,7 +47,6 @@ export default {
           key: "gender",
           render: (h, params) => {
             // 0：男, 1：女
-            // return this.renderRadio(h, params, 'gender', 'tbDataCopy')
             if(params.row.edit) {
               return h('RadioGroup', {
                 props: {
@@ -64,21 +78,89 @@ export default {
           title: "出生日期",
           key: "birthday",
           render: (h, params) => {
-              // return this.renderDate(h, params, 'birthday', 'tbDataCopy')
+            if(params.row.edit) {
+              return h('DatePicker', {
+                props: {
+                  type: 'date',
+                  placeholder: '请选择出生日期',
+                  options: this.dateOption,
+                  value: params.row.birthday,
+                  transfer: true
+                },
+                on: {
+                  'on-change': (val) => {
+                    this.tbDataCopy[params.index].birthday = val
+                  }
+                }
+              })
+            }else {
+              return h('span', {}, params.row.birthday)
+            }
           }
         },
         {
           title: "所在城市",
           key: "city",
           render: (h, params) => {
-              // return this.renderSelect(h, params, 'city', 'tbDataCopy')
+            if(params.row.edit) {
+              let children = [] //模拟处理接口请求的数据
+              this.cityList.forEach(city => {
+                children.push(h('Option', {props: {value: city.value}}, city.label))
+              })
+              return h('Select', {
+                props: {
+                  value: params.row.city,
+                  transfer: true
+                },
+                on: {
+                  'on-change': (val) => {
+                    this.tbDataCopy[params.index].city = val
+                  }
+                }
+              }, children)
+            }else {
+              let cityName = ''
+              this.cityList.forEach(city => {
+                if(city.value === params.row.city) {
+                  cityName = city.label
+                }
+              })
+              return h('span', {}, cityName)
+            }
           }
         },
         {
           title: "爱好",
           key: "hobby",
           render: (h, params) => {
-              // return this.renderMulSelect(h, params, 'hobby', 'tbDataCopy')
+            if(params.row.edit) {
+              let children = [] //模拟处理接口请求的数据
+              this.hobbyList.forEach(hobby => {
+                children.push(h('Option', {props: {value: hobby.value}}, hobby.label))
+              })
+              return h('Select', {
+                props: {
+                  value: params.row.hobby,
+                  transfer: true,
+                  multiple: true
+                },
+                on: {
+                  'on-change': (valList) => {
+                    this.tbDataCopy[params.index].hobby = valList
+                  }
+                }
+              }, children)
+            }else {
+              let hobbys = []
+              params.row.hobby.forEach(item => {
+                this.hobbyList.forEach(hobby => {
+                  if(hobby.value === item) {
+                    hobbys.push(hobby.label)
+                  }
+                })
+              })              
+              return h('span', {}, hobbys.join(','))
+            }
           }
         },
         {
@@ -96,11 +178,11 @@ export default {
                     },
                     on: {
                       click: () => {
-                        //数据改变
+                        //保存数据
                         for (let key in this.tbDataCopy[params.index]) {
                             this.tbData[params.index][key] = this.tbDataCopy[params.index][key];
                         }
-                        params.row.edit = false;
+                        this.tbData[params.index].edit = false;
                       }
                     }
                   },
@@ -116,7 +198,7 @@ export default {
                     },
                     on: {
                       click: () => {
-                        params.row.edit = false;
+                        this.tbData[params.index].edit = false;
                       }
                     }
                   },
@@ -134,7 +216,7 @@ export default {
                     },
                     on: {
                       click: () => {
-                        params.row.edit = true;
+                        this.tbData[params.index].edit = true;
                       }
                     }
                   },
@@ -146,47 +228,177 @@ export default {
         }
       ],
       tbData: [
-        //全市警情
+        //表格数据
         {
           name: '张三',
           gender: 1,
           birthday: '2019/08/29',
-          city: '1',
-          hobby: [],
+          city: 'beijing',
+          hobby: ['movie', 'sing'],
+          edit: false
+        }, 
+        {
+          name: '李四',
+          gender: 0,
+          birthday: '2019/08/30',
+          city: 'shenzhen',
+          hobby: ['sing'],
+          edit: false
+        }, 
+        {
+          name: '小李',
+          gender: 0,
+          birthday: '2019/09/01',
+          city: 'shanghai',
+          hobby: ['movie', 'stroll'],
+          edit: false
+        }, 
+        {
+          name: '小张',
+          gender: 1,
+          birthday: '2019/08/29',
+          city: 'beijing',
+          hobby: ['travel'],
+          edit: false
+        }, 
+        {
+          name: '小徐',
+          gender: 0,
+          birthday: '2019/09/01',
+          city: 'shenzhen',
+          hobby: ['sing'],
+          edit: false
+        }, 
+        {
+          name: '小杨',
+          gender: 1,
+          birthday: '2019/08/29',
+          city: 'beijing',
+          hobby: ['movie', 'sing'],
           edit: false
         },       
       ],
       tbDataCopy: [
-        //全市警情
+        //表格数据副本
         {
           name: '张三',
           gender: 1,
           birthday: '2019/08/29',
-          city: '1',
-          hobby: [],
+          city: 'beijing',
+          hobby: ['movie', 'sing'],
+        },
+        {
+          name: '李四',
+          gender: 0,
+          birthday: '2019/08/30',
+          city: 'shenzhen',
+          hobby: ['sing'],
+        }, 
+        {
+          name: '小李',
+          gender: 0,
+          birthday: '2019/09/01',
+          city: 'shanghai',
+          hobby: ['movie', 'stroll'],
+        }, 
+        {
+          name: '小张',
+          gender: 1,
+          birthday: '2019/08/29',
+          city: 'beijing',
+          hobby: ['travel'],
+        }, 
+        {
+          name: '小徐',
+          gender: 0,
+          birthday: '2019/09/01',
+          city: 'shenzhen',
+          hobby: ['sing'],
+        }, 
+        {
+          name: '小杨',
+          gender: 1,
+          birthday: '2019/08/29',
+          city: 'beijing',
+          hobby: ['movie', 'sing'],
         },
       ],
+      dateOption: {
+        shortcuts: [
+            {
+                text: '近一周',
+                value () {                      
+                    const start = new Date();
+                    start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                    return start;
+                }
+            },
+            {
+                text: '近一月',
+                value () {
+                    const start = new Date();
+                    start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                    return start;
+                }
+            },
+            {
+                text: '近三月',
+                value () {
+                    const start = new Date();
+                    start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                    return start;
+                }
+            }
+        ]
+      },
+      cityList: [
+        {
+          value: 'beijing',
+          label: '北京'
+        },
+        {
+          value: 'shanghai',
+          label: '上海'
+        },
+        {
+          value: 'shenzhen',
+          label: '深圳'
+        }
+      ],
+      hobbyList: [
+        {
+          value: 'movie',
+          label: '电影'
+        },
+        {
+          value: 'travel',
+          label: '旅行'
+        },
+        {
+          value: 'sing',
+          label: 'k歌'
+        },
+        {
+          value: 'stroll',
+          label: '散步'
+        }
+      ]
     };
   },
   methods: {
-    renderInput(h, params, key, copyDataStr) {
-        if (params.row.edit) {
-            return h("Input", {
-                props: {
-                    value: params.row[key],
-                },
-                attrs: {
-                    id: `${key}_${params.index}`
-                },
-                on: {
-                    "on-blur": () => {                      
-                        this[copyDataStr][params.index][key] = document.querySelector(`#${key}_${params.index} input`).value;
-                    }
-                }
-            });
-        } else {
-            return h("span", params.row[key]);
-        }
+    handleSubmit() {
+      this.$Message.success('请于控制台查看输出')
+      let params = []
+      this.tbData.forEach(row => {
+        params.push({
+          name: row.name,
+          gender: row.gender,
+          birthday: row.birthday,
+          city: row.city,
+          hobby: row.hobby,
+        })
+      })
+      console.log('表格数据：', params)
     }
   }
 };
